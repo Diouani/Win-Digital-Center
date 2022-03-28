24c9b428ca14716e65dea95b81f1a1d68b978cd2
Diouani <diouani.youssef@gmail.com>
2022-03-28 09:57:32 +0000

    initialisation du project avec SpringBoot

---
+++ src/main/java/com/wdc/wdc/WinDigitalCenterApplication.java
@@ -0,0 +1,13 @@
+package com.wdc.wdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinDigitalCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinDigitalCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wdc/wdc/WinDigitalCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdc.wdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinDigitalCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
